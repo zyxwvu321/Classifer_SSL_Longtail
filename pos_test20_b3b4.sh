ef5e2117c646693e9e3a67d75df088f182fd7cc3
Minjie Chen <13761573873@163.com>
2020-06-12 18:50:39 +0800

    isic20 pos metafix b3b4 test

---
+++ pos_test20_b3b4.sh
@@ -0,0 +1,12 @@
+
+
+python train_BNN.py --config_file ./configs/effb4_meta_default20_extrameta_w_d_metafix_pos.yaml
+
+python test_gen_classifer.py --config_file ./configs/effb4_meta_default20_extrameta_w_d_metafix_pos_test.yaml  MISC.ONLY_TEST True MISC.TTA True MISC.N_TTA 20
+
+
+
+python train_BNN.py --config_file ./configs/effb3_meta_default20_extrameta_w_d_metafix_pos.yaml
+
+python test_gen_classifer.py --config_file ./configs/effb3_meta_default20_extrameta_w_d_metafix_pos_test.yaml  MISC.ONLY_TEST True MISC.TTA True MISC.N_TTA 20
+
