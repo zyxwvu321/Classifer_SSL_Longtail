e6c09414c49e695b0f4221a3c6245ae3929a1788
Minjie Chen <minjie.chen@weimob.com>
2020-06-24 22:25:50 +0800

    script 20

--- pos_test20_b3b4.sh
+++ pos_test20_b3b4.sh
@@ -1,12 +1,21 @@
 
 
-python train_BNN.py --config_file ./configs/effb4_meta_default20_extrameta_w_d_metafix_pos.yaml
+python train_BNN.py --config_file ./configs/effb3_meta_default20_extrameta_w_d_metafix.yaml
 
-python test_gen_classifer.py --config_file ./configs/effb4_meta_default20_extrameta_w_d_metafix_pos_test.yaml  MISC.ONLY_TEST True MISC.TTA True MISC.N_TTA 20
-
-
+python test_gen_classifer.py --config_file ./configs/effb3_meta_default20_extrameta_w_d_metafix_test.yaml  MISC.ONLY_TEST True MISC.TTA True MISC.N_TTA 20
 
 python train_BNN.py --config_file ./configs/effb3_meta_default20_extrameta_w_d_metafix_pos.yaml
 
 python test_gen_classifer.py --config_file ./configs/effb3_meta_default20_extrameta_w_d_metafix_pos_test.yaml  MISC.ONLY_TEST True MISC.TTA True MISC.N_TTA 20
+
+
+
+python train_BNN.py --config_file ./configs/effb4_meta_default20_extrameta_w_d_metafix.yaml
+
+python test_gen_classifer.py --config_file ./configs/effb4_meta_default20_extrameta_w_d_metafix_test.yaml  MISC.ONLY_TEST True MISC.TTA True MISC.N_TTA 20
+
+
+python train_BNN.py --config_file ./configs/effb4_meta_default20_extrameta_w_d_metafix_pos.yaml
+
+python test_gen_classifer.py --config_file ./configs/effb4_meta_default20_extrameta_w_d_metafix_pos_test.yaml  MISC.ONLY_TEST True MISC.TTA True MISC.N_TTA 20
 
